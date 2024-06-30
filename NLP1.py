1c18b0d0fdc8b010675b0d96d8a05602be556fe4
SilverStark18 <amarlearns18215@gmail.com>
2024-06-30 19:04:24 +0530

    stopwords --removal from sentence

---
+++ NLP1.py
@@ -0,0 +1,9 @@
+# To remove stop words from the given text
+
+from nltk.corpus import stopwords
+stoplist=stopwords.words('english')
+text="Hii, hello Namaste Dosto.NLP stands for NATURAL LANGUAGE PROCESSING "
+clean_words=[word for word in text.split() if word not in stoplist]
+print(text)
+print("NEW text",end="--->")
+print(clean_words)
